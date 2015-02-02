619dfd01fedf6b785bb9c83d9139f3c345d6cd57
miknecht <m_knecht@gmx.de>
2015-02-02 19:30:51 +0100

    Improve

--- statisticalmechanics/src/programs_assessment_1/b2.py
+++ statisticalmechanics/src/programs_assessment_1/b2.py
@@ -1,12 +1,21 @@
 import random
 
 x, y = 1.0, 1.0
-delta = 0.1
-n_trials = 18000
-n_hits = 0
-for i in range(n_trials):
-    del_x, del_y = random.uniform(-delta, delta), random.uniform(-delta, delta)
-    if abs(x + del_x) < 1.0 and abs(y + del_y) < 1.0:
-        x, y = x + del_x, y + del_y
-    if x**2 + y**2 < 1.0: n_hits += 1
-print 4.0 * n_hits / float(n_trials)
+n_trials = 2 ** 12
+
+deltas = [0.062, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0]
+
+print "delta | acceptance rate"
+for delta in deltas:
+    n_hits = 0
+    n_accept = 0
+    for i in range(n_trials):
+        del_x, del_y = random.uniform(-delta, delta), random.uniform(-delta, delta)
+        if abs(x + del_x) < 1.0 and abs(y + del_y) < 1.0:
+            x, y = x + del_x, y + del_y
+            n_accept += 1
+        if x**2 + y**2 < 1.0: 
+            n_hits += 1
+    print delta, "|", n_accept / float(n_trials)
+
+
--- statisticalmechanics/src/programs_assessment_1/c1.py
+++ statisticalmechanics/src/programs_assessment_1/c1.py
@@ -3,6 +3,7 @@ import random, math
 n_trials = 400000
 n_hits = 0
 var = 0.0
+var2 = 0.0
 for i in range(n_trials):
     x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
     Obs = 0.0
@@ -10,4 +11,12 @@ for i in range(n_trials):
         n_hits += 1
         Obs = 4.0
     var += (Obs - math.pi)**2
-print 4.0 * n_hits / float(n_trials), math.sqrt(var / n_trials)
+    var2 += (Obs * Obs - math.pi * math.pi)**2
+
+a = math.sqrt(var / n_trials)
+b = math.sqrt(var2 / n_trials) 
+
+print 4.0 * n_hits / float(n_trials), a, b, b - a*a 
+
+#  <Obs> and <Obs^2>, <Obs^2> - <Obs>^2,
+
