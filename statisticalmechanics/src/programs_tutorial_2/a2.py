462095d123ff6ee6898440bea86fd20a7d0e39c7
miknecht <m_knecht@gmx.de>
2015-02-14 09:22:56 +0100

    Add

---
+++ statisticalmechanics/src/programs_tutorial_2/a2.py
@@ -0,0 +1,57 @@
+import random, math
+
+def direct_disks_box(N, sigma):
+    condition = False
+    while condition == False:
+        L = [(random.uniform(sigma, 1.0 - sigma), random.uniform(sigma, 1.0 - sigma))]
+        for k in range(1, N):
+            a = (random.uniform(sigma, 1.0 - sigma), random.uniform(sigma, 1.0 - sigma))
+            min_dist = min(math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) for b in L)
+            if min_dist < 2.0 * sigma:
+                condition = False
+                break
+            else:
+                L.append(a)
+                condition = True
+    return L
+
+
+def markov_disks_box(N, sigma):
+    L = [[0.25, 0.25], [0.75, 0.25], [0.25, 0.75], [0.75, 0.75]]
+    sigma_sq = sigma ** 2
+    delta = 0.1
+    for steps in range(N):
+        a = random.choice(L)
+        b = [a[0] + random.uniform(-delta, delta), a[1] + random.uniform(-delta, delta)]
+        min_dist = min((b[0] - c[0]) ** 2 + (b[1] - c[1]) ** 2 for c in L if c != a)
+        box_cond = min(b[0], b[1]) < sigma or max(b[0], b[1]) > 1.0 - sigma
+        if not (box_cond or min_dist < 4.0 * sigma ** 2):
+            a[:] = b
+    return L
+
+#sigma = 0.15
+#del_xy = 0.05
+
+sigma = 0.15
+#del_xy = 0.10
+del_xy = 0.05
+#n_runs = 1000000
+n_runs =  10000
+conf_a = ((0.30, 0.30), (0.30, 0.70), (0.70, 0.30), (0.70,0.70))
+conf_b = ((0.20, 0.20), (0.20, 0.80), (0.75, 0.25), (0.75,0.75))
+conf_c = ((0.30, 0.20), (0.30, 0.80), (0.70, 0.20), (0.70,0.70))
+configurations = [conf_a, conf_b, conf_c]
+hits = {conf_a: 0, conf_b: 0, conf_c: 0}
+for run in range(n_runs):
+#    x_vec = direct_disks_box(4, sigma)
+    x_vec = markov_disks_box(4, sigma)
+    for conf in configurations:
+        condition_hit = True
+        for b in conf:
+            condition_b = min(max(abs(a[0] - b[0]), abs(a[1] - b[1])) for a in x_vec) < del_xy
+            condition_hit *= condition_b
+        if condition_hit:
+            hits[conf] += 1
+for a in hits:
+    print a, hits[a]
+
