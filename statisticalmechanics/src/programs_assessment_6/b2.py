8fc553a7002e59af841260665c69f7f1c7358d5e
miknecht <m_knecht@gmx.de>
2015-03-16 17:48:21 +0100

    Ass

--- statisticalmechanics/src/programs_assessment_6/b2.py
+++ statisticalmechanics/src/programs_assessment_6/b2.py
@@ -3,12 +3,27 @@ import math, random, pylab
 def rho_free(x, y, beta):
     return math.exp(-(x - y) ** 2 / (2.0 * beta))
 
+def levy_harmonic_path(xstart, xend, dtau, N):
+    x = [xstart]
+    for k in range(1, N):
+        dtau_prime = (N - k) * dtau
+        Ups1 = 1.0 / math.tanh(dtau) + \
+               1.0 / math.tanh(dtau_prime)
+        Ups2 = x[k - 1] / math.sinh(dtau) + \
+               xend / math.sinh(dtau_prime)
+        x.append(random.gauss(Ups2 / Ups1, \
+               1.0 / math.sqrt(Ups1)))
+    return x
+
+
 beta = 20.0
 N = 80
+Ncut = N/2
 dtau = beta / N
 delta = 1.0
 n_steps = 4000000
 x = [5.0] * N
+#x = levy_harmonic_path(x[0], x[0], dtau, N)
 data = []
 for step in range(n_steps):
     k = random.randint(0, N - 1)
@@ -26,13 +41,15 @@ for step in range(n_steps):
         k = random.randint(0, N - 1)
         data.append(x[k])
 
-list_x = x
-list_y = [ i * beta / N for i in range(N)]
-pylab.plot(list_x, list_y, label='path')
+pylab.hist(data, normed=True, bins=100, label='QMC')
+list_x = [0.1 * a for a in range (-30, 31)]
+list_y = [math.sqrt(math.tanh(beta / 2.0)) / math.sqrt(math.pi) * \
+          math.exp(-x ** 2 * math.tanh(beta / 2.0)) for x in list_x]
+pylab.plot(list_x, list_y, label='analytic')
 pylab.legend()
 pylab.xlabel('$x$')
-pylab.ylabel('imaginary time')
+pylab.ylabel('$\\pi(x)$ (normalized)')
 pylab.title('naive_harmonic_path (beta=%s, N=%i)' % (beta, N))
 pylab.xlim(-2, 2)
-pylab.savefig('plot_path_beta%s.png' % beta)
+pylab.savefig('plot_B1_beta%s.png' % beta)
 pylab.show()
