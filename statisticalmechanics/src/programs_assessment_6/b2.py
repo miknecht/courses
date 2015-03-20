e260013351fd209b1d3449d27dd3b6f915144657
miknecht <m_knecht@gmx.de>
2015-03-20 13:45:47 +0100

    Add

--- statisticalmechanics/src/programs_assessment_6/b2.py
+++ statisticalmechanics/src/programs_assessment_6/b2.py
@@ -18,28 +18,18 @@ def levy_harmonic_path(xstart, xend, dtau, N):
 
 beta = 20.0
 N = 80
-Ncut = N/2
+Ncut = N / 2
 dtau = beta / N
 delta = 1.0
-n_steps = 4000000
+n_steps = 400000
+
 x = [5.0] * N
-#x = levy_harmonic_path(x[0], x[0], dtau, N)
 data = []
 for step in range(n_steps):
+    x = levy_harmonic_path(x[0], x[0], dtau, N)
+    x = x[Ncut:] + x[:Ncut]
     k = random.randint(0, N - 1)
-    knext, kprev = (k + 1) % N, (k - 1) % N
-    x_new = x[k] + random.uniform(-delta, delta)
-    old_weight  = (rho_free(x[knext], x[k], dtau) *
-                   rho_free(x[k], x[kprev], dtau) *
-                   math.exp(-0.5 * dtau * x[k] ** 2))
-    new_weight  = (rho_free(x[knext], x_new, dtau) *
-                   rho_free(x_new, x[kprev], dtau) *
-                   math.exp(-0.5 * dtau * x_new ** 2))
-    if random.uniform(0.0, 1.0) < new_weight / old_weight:
-        x[k] = x_new
-    if step % N == 0:
-        k = random.randint(0, N - 1)
-        data.append(x[k])
+    data.append(x[k])
 
 pylab.hist(data, normed=True, bins=100, label='QMC')
 list_x = [0.1 * a for a in range (-30, 31)]
@@ -49,7 +39,10 @@ pylab.plot(list_x, list_y, label='analytic')
 pylab.legend()
 pylab.xlabel('$x$')
 pylab.ylabel('$\\pi(x)$ (normalized)')
-pylab.title('naive_harmonic_path (beta=%s, N=%i)' % (beta, N))
+pylab.title('levy_harmonic_path (beta=%s, N=%i)' % (beta, N))
 pylab.xlim(-2, 2)
 pylab.savefig('plot_B1_beta%s.png' % beta)
 pylab.show()
+
+
+
